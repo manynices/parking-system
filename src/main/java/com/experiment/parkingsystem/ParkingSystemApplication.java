691c6d16ab13e6491682f8c20eef9a774ba0397f
newfire06 <2207980762@qq.com>
2025-11-03 12:07:14 +0800

    初始化

--- src/main/java/com/experiment/parkingsystem/ParkingSystemApplication.java
+++ src/main/java/com/experiment/parkingsystem/ParkingSystemApplication.java
@@ -3,13 +3,13 @@ package com.experiment.parkingsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLOutput;
+
 
 @SpringBootApplication
 public class ParkingSystemApplication {
 
     public static void main(String[] args) {
-
+        System.out.println("do it");
         SpringApplication.run(ParkingSystemApplication.class, args);
     }
 
