4485f36a3199705560dab8e443fd3b9639078c8b
newfire06 <2207980762@qq.com>
2025-11-10 17:08:38 +0800

    第一模块业主信息增删改查

--- src/main/java/com/experiment/parkingsystem/ParkingSystemApplication.java
+++ src/main/java/com/experiment/parkingsystem/ParkingSystemApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParkingSystemApplication {
 
     public static void main(String[] args) {
-        System.out.println("do it");
         SpringApplication.run(ParkingSystemApplication.class, args);
     }
 
