f4173512e8ee42f0c3db9ddee1e426c5bb55dc9a
newfire06 <2207980762@qq.com>
2025-11-17 09:28:19 +0800

    第二模块车辆信息增删改查

--- src/main/java/com/experiment/parkingsystem/ParkingSystemApplication.java
+++ src/main/java/com/experiment/parkingsystem/ParkingSystemApplication.java
@@ -1,11 +1,15 @@
 package com.experiment.parkingsystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
 @SpringBootApplication
+@MapperScan({
+        "com.experiment.parkingsystem.mapper" // 扫描 OwnerMapper 和 VehicleMapper
+})
 public class ParkingSystemApplication {
 
     public static void main(String[] args) {
