427a79f881b8ae638c9bca89bcf9a74e38e30842
newfire06 <2207980762@qq.com>
2025-11-24 12:45:34 +0800

    修改汽车模块部分功能

--- src/main/java/com/experiment/parkingsystem/mapper/VehicleMapper.java
+++ src/main/java/com/experiment/parkingsystem/mapper/VehicleMapper.java
@@ -10,16 +10,16 @@ public interface VehicleMapper {
 
     int insert(Vehicle vehicle);
 
-    Vehicle findById(@Param("vehicleId") String vehicleId);
+    Vehicle findById(@Param("licensePlate") String licensePlate);
 
     // 查询列表，根据 ownerId 和 status 进行过滤
     List<Vehicle> findByCriteria(@Param("ownerId") String ownerId, @Param("status") String status);
 
     int update(Vehicle vehicle);
 
-    int deleteById(@Param("vehicleId") String vehicleId);
+    int deleteById(@Param("licensePlate") String licensePlate);
 
-    int existsById(@Param("vehicleId") String vehicleId);
+    int existsById(@Param("licensePlate") String licensePlate);
 
     // 用于生成新的 vehicleId
     String findMaxVehicleId();
